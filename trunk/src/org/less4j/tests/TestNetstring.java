1ac7cc16978d03b1b3a5151bb139b496f70c866d
laurentszyster <laurentszyster@6f7d06a6-901f-0410-aee2-87374a33d76d>
2007-06-12 22:46:03 +0000

    Preparing tests for interleaved requests and responses

--- trunk/src/org/less4j/tests/TestNetstring.java
+++ trunk/src/org/less4j/tests/TestNetstring.java
@@ -7,26 +7,42 @@ import java.util.Iterator;
 
 
 public class TestNetstring {
+    
+    private static final String _utf8 = "UTF-8";
 
     /**
      * Try to connect ansql2 server on TCP port 3999 at address 127.0.0.2
      * and pipeline two constant statements to be encoded as UTF-8.
      * 
+     * Note that there is no way to pipeline
+     * 
      * @param args
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
+        // open a TCP/IP socket to a local's ansql2 server
         Socket conn = new Socket("127.0.0.2", 3999);
+        // instanciate an iterator through netstrings received
+        Iterator recv = Netstring.recv(conn, 16384, _utf8);
         try {
+            // send two netstrings encoded in UTF-8
             Netstring.send(
-                conn, "[\"CREATE TABLE relation (s, p, o, c)\", []]", "UTF-8"
+                conn, "[\"CREATE TABLE relation (s, p, o, c)\", []]", _utf8
                 );
             Netstring.send(
-                conn, "[\"SELECT * FROM relation\", []]", "UTF-8"
+                conn, "[\"SELECT * FROM relation\", []]", _utf8
                 );
-            Iterator netstring = Netstring.recv(conn, 16384, "UTF-8");
-            while (netstring.hasNext())
-                System.out.println (netstring.next());
+            // receive two netstrings decoded from UTF-8
+            System.out.println (recv.next());
+            System.out.println (recv.next());
+            // interleave requests and responses 
+            String request = "[\"SELECT * FROM relation\", []]"; 
+            Netstring.send(conn, request, _utf8);
+            Netstring.send(conn, request, _utf8);
+            System.out.println (recv.next());
+            Netstring.send(conn, request, _utf8);
+            System.out.println (recv.next());
+            System.out.println (recv.next());
         } finally {
             conn.close ();
         }
